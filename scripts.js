018a364549c678012c5ca33b7bf7cad0a4765930
Ayrton Yamashita <ayrtonyamashita@gmail.com>
2024-08-12 10:36:10 -0400

    Atualizando valor de moeda no input

--- scripts.js
+++ scripts.js
@@ -1,7 +1,25 @@
 const ipt_amount = document.querySelector("#amount")
 
-
+// Captura o evento de input para formatar o valor.
 ipt_amount.oninput = () => {
+  // Captura o valor do input e remove caracteres não númericos
   let value = ipt_amount.value.replace(/\D/g, "");
-  ipt_amount.value = value
+
+  // Transformar o valor em centavos (exemplo: 150/100 = 1.5 que é equivalente a R$ 1,50)
+  value = Number(value) / 100
+
+  // Atualiza o valor do input sem caracteres não numéricos.
+  ipt_amount.value = formatCurrentBRL(value)
+}
+
+function formatCurrentBRL(value){
+
+  // Formata o valor no padrão BRL REAL BRASILEIRO
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+
+  // Retorna o valor formatado
+  return value
 }
